dca5b437acdef4790ec7478d4f1a2b9e1d2a03a1
@vadim17 <vadim@genesort.com>
2023-04-01 15:50:32 +0300

    commit

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
-import Darts from './src/darts';
-import {name as appName} from './app.json';
-
-AppRegistry.registerComponent(appName, () => Darts);
+import './src/index';
--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,18 @@
-import './src/index';
+/**
+ * @format
+ */
+import React from 'react';
+import {AppRegistry} from 'react-native';
+import Darts from './darts';
+import {name as appName} from '../app.json';
+import {Provider} from 'react-redux';
+import {store} from './store';
+
+const initWrapper = () => {
+  return (
+    <Provider store={store}>
+      <Darts />
+    </Provider>
+  );
+};
+AppRegistry.registerComponent(appName, () => initWrapper);
