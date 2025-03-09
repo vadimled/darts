124f5daeaf2f8150ae12968c86ed808a3c965b1b
@vadim17 <vadim@genesort.com>
2025-03-09 12:49:51 +0200

    upgrade to 76.7

--- babel.config.js
+++ babel.config.js
@@ -1,21 +1,17 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+  presets: ['module:@react-native/babel-preset'],
   plugins: [
     [
       'module-resolver',
       {
-        root: './',
-        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        root: ['./'],
+        extensions: ['.ios.js', '.android.js', '.js', '.jsx', '.ts', '.tsx', '.json'],
         alias: {
-          tests: ['./tests/'],
           '@components': ['./src/components'],
           '@screens': ['./src/screens'],
         },
       },
     ],
     'react-native-reanimated/plugin',
-    ['@babel/plugin-transform-private-methods', { loose: true }], // Добавляем loose: true
-    ['@babel/plugin-transform-class-properties', { loose: true }], // Добавляем loose: true
-    ['@babel/plugin-transform-private-property-in-object', { loose: true }] // Добавляем loose: true
   ],
 };
--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,15 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+// metro.config.js
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('metro-config').MetroConfig}
- */
-const config = {};
-
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+module.exports = (async () => {
+  const defaultConfig = await getDefaultConfig(__dirname);
+  
+  return mergeConfig(defaultConfig, {
+    resolver: {
+      sourceExts: [...defaultConfig.resolver.sourceExts, 'ts', 'tsx'],
+    },
+    transformer: {
+      assetRegistryPath: 'react-native/Libraries/Image/AssetRegistry',
+    },
+  });
+})();
